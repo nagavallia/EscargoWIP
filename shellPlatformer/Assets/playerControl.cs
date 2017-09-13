a8bc557bf69ad5dc0f00654343434aeabe08b550
NickTeo96 <nt269@cornell.edu>
2017-09-13 17:48:29 -0400

    added very simple snail with movement and hole in the middle

---
+++ shellPlatformer/Assets/playerControl.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerControl : MonoBehaviour
+{
+	[HideInInspector]
+	public bool jump = false;				// Condition for whether the player should jump.
+
+
+	public float moveForce = 365f;			// Amount of force added to move the player left and right.
+	public float maxSpeed = 5f;				// The fastest the player can travel in the x axis.
+	public float jumpForce = 1000f;			// Amount of force added when the player jumps.
+	private Transform groundCheck;			// A position marking where to check if the player is grounded.
+
+	private bool grounded = false;			// Whether or not the player is grounded.
+
+
+	void Awake()
+	{
+		// Setting up references.
+		groundCheck = transform.Find("groundCheck");
+	}
+
+
+	void Update()
+	{
+//		// The player is grounded if a linecast to the groundcheck position hits anything on the ground layer.
+//		grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));  
+
+		// If the jump button is pressed and the player is grounded then the player should jump.
+		if(Input.GetButtonDown("Jump"))
+			jump = true;
+	}
+
+
+	void FixedUpdate ()
+	{
+		// Cache the horizontal input.
+		float h = Input.GetAxis("Horizontal");
+
+		// If the player is changing direction (h has a different sign to velocity.x) or hasn't reached maxSpeed yet...
+		if(h * GetComponent<Rigidbody2D>().velocity.x < maxSpeed)
+			// ... add a force to the player.
+			GetComponent<Rigidbody2D>().AddForce(Vector2.right * h * moveForce);
+
+		// If the player's horizontal velocity is greater than the maxSpeed...
+		if(Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x) > maxSpeed)
+			// ... set the player's velocity to the maxSpeed in the x axis.
+			GetComponent<Rigidbody2D>().velocity = new Vector2(Mathf.Sign(GetComponent<Rigidbody2D>().velocity.x) * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
+
+		// If the player should jump...
+		if(jump)
+		{
+			// Add a vertical force to the player.
+			GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce));
+
+			// Make sure the player can't jump again until the jump conditions from Update are satisfied.
+			jump = false;
+		}
+	}
+}
