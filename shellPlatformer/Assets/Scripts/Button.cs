69c165b4d063d02cfe0c3ee807ab85d333aee573
James McManus <jjm439@cornell.edu>
2017-09-18 09:26:52 -0400

    button spawn in exit on collision

--- shellPlatformer/Assets/Scripts/Button.cs
+++ shellPlatformer/Assets/Scripts/Button.cs
@@ -3,19 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Button : MonoBehaviour {
-	[SerializeField] private GameObject exit;
+	public GameObject exit;
 
 	// Use this for initialization
 	void Start () {
-
+//		exit = Resources.Load ("Exit") as GameObject;
 	}
 
 	// Update is called once per frame
-	void Update () {
-
+	void Update () 
+	{
 	}
 
-	private void OnTriggerEnter2D(Collider2D collision) {  
-		Instantiate(exit, new Vector3 (5f, 0f, 0f), transform.rotation);
+	private void OnTriggerEnter2D(Collider2D collision) {
+		Instantiate (exit, new Vector3 (5f, 0f, 0f), Quaternion.identity);
 	}  
 }
