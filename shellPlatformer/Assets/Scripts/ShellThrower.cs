0d54a64b9eb747f70cb17155b606204d2e9d7b60
Arnesh Nagavalli <amn79@cornell.edu>
2017-09-18 11:51:27 -0400

    shell throwing feels better

--- shellPlatformer/Assets/Scripts/ShellThrower.cs
+++ shellPlatformer/Assets/Scripts/ShellThrower.cs
@@ -17,6 +17,7 @@ public class ShellThrower : MonoBehaviour {
 
     private Transform player;
     [SerializeField] private Vector3 defaultShellPos;
+    [SerializeField] private float shellPickupSpeed;
     [SerializeField] private float interactDist;
 
 	// Use this for initialization
@@ -31,6 +32,8 @@ public class ShellThrower : MonoBehaviour {
         shellRigidBody = shell.GetComponent<Rigidbody2D>();
         player = GameObject.FindWithTag("Player").transform;
 
+        Physics2D.IgnoreCollision(player.GetComponent<Collider2D>(), shellHitbox);
+
         if (transform.parent == null) {
             ReleaseShell();
         } else {
@@ -40,9 +43,11 @@ public class ShellThrower : MonoBehaviour {
 
     private void Update() {
         if (Input.GetKeyDown(KeyCode.E)) {
-            if (transform.parent == null && shellRigidBody.velocity.magnitude < 1f  
+            if (transform.parent == null && shellRigidBody.velocity.magnitude < shellPickupSpeed  
                 && (player.position - transform.position).magnitude < interactDist) {
                 PickUpShell();
+            } else if (transform.parent != null && transform.parent.GetComponent<PlayerController>() != null) {
+                ReleaseShell();
             }
         }
     }
@@ -82,6 +87,7 @@ public class ShellThrower : MonoBehaviour {
         shellHitbox.enabled = false;
         shellClickbox.enabled = true;
         shellRigidBody.isKinematic = true;
+        shellRigidBody.velocity = Vector3.zero;
         shell.transform.localPosition = defaultShellPos;
     }
 
