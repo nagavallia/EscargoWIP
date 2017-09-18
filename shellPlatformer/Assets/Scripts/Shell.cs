28e45db79a092b4bbf632e346d40920a7e5cf974
Grant Gonyer <gcg45@cornell.edu>
2017-09-18 00:15:20 -0400

    Shell now returns to spawner on destroy

--- shellPlatformer/Assets/Scripts/Shell.cs
+++ shellPlatformer/Assets/Scripts/Shell.cs
@@ -20,5 +20,6 @@ public class Shell : MonoBehaviour {
         // do code here for when the shell should be destroyed
         // probably broadcast a SHELL_DESTROYED message that will be intercepted by a shell spawner
 		this.gameObject.transform.SetParent(shellSpawner.transform);
+		this.gameObject.transform.localPosition = new Vector3 (0f, 0f, 0);
     }
 }
