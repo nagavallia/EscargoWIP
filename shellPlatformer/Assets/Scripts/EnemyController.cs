d8a40efdd00654d1dab07b05e747050bb3838b7b
Arnesh Nagavalli <amn79@cornell.edu>
2017-09-24 10:42:22 -0400

    think the EnemyController thing is done now, hopefully

--- shellPlatformer/Assets/Scripts/EnemyController.cs
+++ shellPlatformer/Assets/Scripts/EnemyController.cs
@@ -9,57 +9,63 @@ public class EnemyController : MonoBehaviour {
     private float direction;
     private Vector3 startPos;
 
-    public bool rotate;
+	public bool rotate;
 
-    private Quaternion toRotation = Quaternion.Euler(0, 0, -90);
-    private Quaternion fromRotation = Quaternion.Euler(0, 0, 0);
-    public bool toggle = true;
-    public float t = 0f;
-    public float duration = 5f;
+	private Quaternion toRotation = Quaternion.Euler(0,0,-90);
+	private Quaternion fromRotation = Quaternion.Euler(0,0,0);
+	public bool toggle = true;
+	public float t = 0f;
+	public float duration = 5f;
 
-    // Use this for initialization
-    void Start() {
+	// Use this for initialization
+	void Start () 
+	{
         curMove = 0f;
         direction = -1f;
         startPos = transform.localPosition;
-    }
-
-    // Update is called once per frame
-    void Update() {
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
         float movement = direction * speed * Time.deltaTime;
         this.gameObject.transform.Translate(0f, movement, 0f);
         curMove += movement;
-        if (curMove < -1f * moveDist) {
-            this.gameObject.transform.localPosition = startPos + new Vector3(0f, -1f * moveDist, 0f);
-            direction = 1f;
-            curMove = -1f * moveDist;
-        } else if (curMove > moveDist) {
-            this.gameObject.transform.localPosition = startPos + new Vector3(0f, moveDist, 0f);
-            direction = -1f;
-            curMove = moveDist;
-        }
+        if (curMove < -1f*moveDist) 
+		{ 
+			this.gameObject.transform.localPosition = startPos + new Vector3(0f, -1f*moveDist, 0f); 
+			direction = 1f; 
+			curMove = -1f * moveDist; 
+		}
+        else if (curMove > moveDist) 
+		{ 
+			this.gameObject.transform.localPosition = startPos + new Vector3(0f, moveDist, 0f); 
+			direction = -1f; 
+			curMove = moveDist; 
+		}
 
-        if (rotate) {
-            if (transform.rotation.z >= fromRotation.z) {
-                toggle = true;
-                t = 0f;
-            } else if (transform.rotation.z <= toRotation.z) {
-                toggle = false;
-                t = 0f;
-            }
-            t += Time.deltaTime;
+		if (rotate) {
+			if (transform.rotation.z >= fromRotation.z) {
+				toggle = true;
+				t = 0f;
+			} else if (transform.rotation.z <= toRotation.z) {
+				toggle = false;
+				t = 0f;
+			} 
+			t += Time.deltaTime;
 
-            if (toggle) {
-                transform.rotation = Quaternion.Lerp(fromRotation, toRotation, t / duration);
-            } else {
-                transform.rotation = Quaternion.Lerp(toRotation, fromRotation, t / duration);
-            }
+			if (toggle) {
+				transform.rotation = Quaternion.Lerp (fromRotation, toRotation, t/duration);
+			} else {
+				transform.rotation = Quaternion.Lerp (toRotation, fromRotation, t/duration);
+			}
 
 
-        }
-    }
+    	}
+	}
 
-    private void OnCollisionEnter2D(Collision2D collision) {
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
         collision.gameObject.SendMessage("KillPlayer", SendMessageOptions.DontRequireReceiver);
     }
 
@@ -68,5 +74,5 @@ public class EnemyController : MonoBehaviour {
         shell.SendMessage("ShellDestroy");
         Destroy(this.gameObject);
     }
-
+		
 }
