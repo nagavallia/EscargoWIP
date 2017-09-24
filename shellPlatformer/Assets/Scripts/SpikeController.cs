e3d7ef3ba48545927e6cd0d1d885f22690779d5c
Grant Clifford Gonyer <gcg45@cornell.edu>
2017-09-24 14:12:33 -0400

    added spike prefab and spikecontroller, working on momentum throw

---
+++ shellPlatformer/Assets/Scripts/SpikeController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D (Collider2D collision) 
+	{
+		collision.gameObject.SendMessage("KillPlayer", this.gameObject, SendMessageOptions.DontRequireReceiver);
+	}
+}
