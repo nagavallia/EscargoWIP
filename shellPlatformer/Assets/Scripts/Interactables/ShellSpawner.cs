7f60cfd8f63558dba12809b603a8c8bf4a6dd76d
Grant Clifford Gonyer <gcg45@en-cs-lgg33-26.coecis.cornell.edu>
2017-10-01 19:28:10 -0400

    working on shellspawner

--- shellPlatformer/Assets/Scripts/Interactables/ShellSpawner.cs
+++ shellPlatformer/Assets/Scripts/Interactables/ShellSpawner.cs
@@ -5,14 +5,27 @@ public class ShellSpawner : MonoBehaviour
 {
 	private GameObject shell;
 	private BoxCollider2D spawnerHitbox;
-	private PlayerController player;
+	private GameObject player;
+
+	// Use this for initialization
+	void Start ()
+	{
+		player = GameObject.Find ("Snail");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+	}
 
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
-		/*if (collision.gameObject.tag == "Player" && this.transform.Find ("Shell") != null) {
-			shell = this.transform.Find("Shell").gameObject;
-			shell.GetComponent<ShellThrower>().PickUpShell();
-		}*/
+		if (collision.gameObject == player) {
+			shell = GameObject.Find ("Shell");
+			shell.transform.SetParent (player.transform);
+			shell.transform.localPosition = new Vector2 (0, 0);
+		}
 	}	
 }
 
