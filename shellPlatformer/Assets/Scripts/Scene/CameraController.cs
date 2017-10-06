a1d7a5d57780506754ca0738deffcb36cc592287
NickTeo96 <nt269@cornell.edu>
2017-10-05 22:09:52 -0400

    camera movement fixes

--- shellPlatformer/Assets/Scripts/Scene/CameraController.cs
+++ shellPlatformer/Assets/Scripts/Scene/CameraController.cs
@@ -8,18 +8,29 @@ public class CameraController : MonoBehaviour {
 
 	private Vector3 offset; // Private variable for the offset of the distance between camera and player
 
+	Vector3 velocity = Vector3.zero; // Velocity of camera
+
+	public float smoothTime = .15f; // The time to move the camera to player position
 
 	// Use this for initialization
 	void Start () {
 		// Set the offset of the camera by calculating the distance 
 		offset = transform.position - player.transform.position;
-	}
-	
-	// LateUpdate is called after Update each frame
-	void Update () {
 
 		// Set the camera position to the player position with the calculated offset distance
-		transform.position = player.transform.position + offset;
+		transform.position = player.transform.position + new Vector3(0,0,offset.z);
+	}
+
+	void FixedUpdate () {
+
+		transform.position = Vector3.SmoothDamp(transform.position, player.transform.position + new Vector3 (0, 0, offset.z), ref velocity , smoothTime);
+
+
+//		if (Mathf.Abs(player.transform.position.x - transform.position.x) > 2
+//			|| Mathf.Abs(player.transform.position.y - transform.position.y) > 2) {
+//			transform.position = player.transform.position + new Vector3(0,0,offset.z);
+//			transform.position = Vector3.SmoothDamp(transform.position, player.transform.position + new Vector3 (0, 0, offset.z), ref velocity , smoothTime);
+//		}
 
 		
 	}
