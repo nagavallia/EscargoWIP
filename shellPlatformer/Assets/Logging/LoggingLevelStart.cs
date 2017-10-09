7022ebb3723787a2566906169b9bbcac815070ce
NickTeo96 <nt269@cornell.edu>
2017-10-08 22:00:51 -0400

    adding logging initialize, page load, and level start and end. However, on death we restart the scene so there are errors with level recording for now

---
+++ shellPlatformer/Assets/Logging/LoggingLevelStart.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoggingLevelStart : MonoBehaviour {
+
+	private int level = 0;
+
+	// Use this for initialization
+	void Start () {
+		Scene scene = SceneManager.GetActiveScene();
+
+
+		if (scene.name == "easy_level") {
+			level = 1;
+
+		} else if (scene.name == "medium_level") {
+			level = 2;
+
+		} else if (scene.name == "hard_level"){
+			level = 3;
+		}
+
+		// Logging record the start of the level with number level
+		LoggingManager.instance.RecordLevelStart (level);
+	}
+}
