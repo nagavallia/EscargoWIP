6e0377b18394e15410b1b9c73392ce2a7e5e7cd0
Arnesh Nagavalli <amn79@cornell.edu>
2017-10-12 14:45:28 -0400

    startup scene

---
+++ shellPlatformer/Assets/Scripts/Managers.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Managers : MonoBehaviour {
+    public static LoggingManager logging { get; private set; }
+    public static SceneController scene { get; private set; }
+
+    private List<GameManager> _startSequence;
+
+    private void Awake() {
+        DontDestroyOnLoad(gameObject);
+
+        logging = GetComponent<LoggingManager>();
+        scene = GetComponent<SceneController>();
+
+        _startSequence = new List<GameManager>();
+        _startSequence.Add(logging);
+        _startSequence.Add(scene);
+
+        StartCoroutine(StartManagers());
+
+        
+    }
+
+    private IEnumerator StartManagers() {
+        foreach (GameManager manager in _startSequence) manager.Startup();
+
+        yield return null;
+
+        scene.FinishLevel();
+    }
+}
