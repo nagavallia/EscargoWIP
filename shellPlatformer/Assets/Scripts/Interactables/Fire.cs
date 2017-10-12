b9636262acbe119b98b5fedd15a29f6dd6d311ff
Grant Gonyer <gcg45@cornell.edu>
2017-10-11 23:34:05 -0400

    Fire water interaction. Refactored spawn shell now listens for a shell_destroyed signal

--- shellPlatformer/Assets/Scripts/Interactables/Fire.cs
+++ shellPlatformer/Assets/Scripts/Interactables/Fire.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class Fire : MonoBehaviour {
 
-	void OnTriggerEnter2D (Collider2D collision) 
+	void OnTriggerEnter2D (Collider2D collision)
 	{
-		if (collision.gameObject.tag == "Player" || collision.gameObject.tag == "NPC") {
+		if (collision.gameObject.tag == "Player" || collision.gameObject.tag == "NPC") 
+		{
 			gameObject.SetActive (false);
+		} 
+		else if (collision.gameObject.tag == "Shell") 
+		{
+			if (collision.gameObject.GetComponent<Shell> ().waterLevel > 0) {
+				gameObject.SetActive (false);
+				collision.gameObject.SendMessage ("ShellDestroy", SendMessageOptions.DontRequireReceiver);
+			}
 		}
 	}
 }
