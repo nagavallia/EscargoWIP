aa709db341a5520c3b5ef2a105aac3643a9ccc4b
Arnesh Nagavalli <amn79@cornell.edu>
2017-10-21 18:30:46 -0400

    untracked

---
+++ shellPlatformer/Assets/Scripts/Platforms/TiledPlatform.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TiledPlatform : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+        BoxCollider2D hitbox = GetComponent<BoxCollider2D>();
+
+        hitbox.size = sprite.size;
+        
+	}
+}
