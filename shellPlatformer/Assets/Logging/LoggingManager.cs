908d6d9eb3edcb638aa15db12311ab1b9487825f
Arnesh Nagavalli <amn79@cornell.edu>
2017-10-21 18:30:29 -0400

    updates

--- shellPlatformer/Assets/Logging/LoggingManager.cs
+++ shellPlatformer/Assets/Logging/LoggingManager.cs
@@ -131,7 +131,7 @@ public class LoggingManager : MonoBehaviour, GameManager
         UnityWebRequest www = UnityWebRequest.Get(pageHost + phpPath + playerActionPath + requestData);
         yield return www.Send();
 
-        if (www.isError)
+        if (www.isNetworkError)
         {
             Debug.Log(www.error);
         }
@@ -164,7 +164,7 @@ public class LoggingManager : MonoBehaviour, GameManager
         UnityWebRequest www = UnityWebRequest.Get(pageHost + phpPath + playerQuestEndPath + requestData);
         yield return www.Send();
 
-        if (www.isError)
+        if (www.isNetworkError)
         {
             Debug.Log(www.error);
         }
@@ -202,7 +202,7 @@ public class LoggingManager : MonoBehaviour, GameManager
         UnityWebRequest www = UnityWebRequest.Get(pageHost + phpPath + playerQuestPath + requestData);
         yield return www.Send();
 
-        if (www.isError)
+        if (www.isNetworkError)
         {
             Debug.Log(www.error);
         }
@@ -239,7 +239,7 @@ public class LoggingManager : MonoBehaviour, GameManager
         UnityWebRequest www = UnityWebRequest.Get(pageHost + phpPath + pageLoadPath + requestData);
         yield return www.Send();
 
-        if (www.isError)
+        if (www.isNetworkError)
         {
             Debug.Log(www.error);
         }
