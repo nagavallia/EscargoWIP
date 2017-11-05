67fcec1ca38074ae322f17c4047c84223243a476
NickTeo96 <nt269@cornell.edu>
2017-11-04 21:57:28 -0400

    Changed version id from 1 to 2 in preparation for newgrounds

--- shellPlatformer/Assets/Logging/LoggingManager.cs
+++ shellPlatformer/Assets/Logging/LoggingManager.cs
@@ -13,7 +13,7 @@ public class LoggingManager : MonoBehaviour, GameManager
     [SerializeField] private bool isDebugging = true; // A convenience parameter which, when set to TRUE, disables logging. 
                                                       // Make sure you set this to FALSE before you submit your game online!
     private int gameId = -1; // The game's specific ID number
-    private int versionId = 1; // Your game's current version number. You should change this number between releases, 
+    private int versionId = 2; // Your game's current version number. You should change this number between releases, 
                                // and after very large changes to your logging methods.
 
     private bool isLevelStarted = false; // Semaphore for assertion
