a1291f0513425fdf119d65c9a8785bea783e89e2
NickTeo96 <nt269@cornell.edu>
2017-11-04 20:02:42 -0400

    privacy policy pop up added in:

---
+++ shellPlatformer/Assets/Scripts/PrivacyPolicy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrivacyPolicy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void showPrivacyPolicy(){
+		GameObject pp = (GameObject)Instantiate (Resources.Load ("PrivacyPolicy"));
+		pp.transform.SetParent (transform.parent);
+		pp.transform.position = transform.parent.position;
+	}
+
+	public void destroyPrivacyPolicy(){
+		Destroy (this.gameObject);
+	}
+}
