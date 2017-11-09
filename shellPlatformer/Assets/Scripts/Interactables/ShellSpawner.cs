ef8b4f92ade0cc4145ac91232fc83a4855c54612
Grant Gonyer <gcg45@cornell.edu>
2017-11-09 14:53:42 -0500

    Level 3 done, cleaned up grant_sandboxes, added mode to shell spawner to either spawn new shells or return your shell

--- shellPlatformer/Assets/Scripts/Interactables/ShellSpawner.cs
+++ shellPlatformer/Assets/Scripts/Interactables/ShellSpawner.cs
@@ -3,17 +3,19 @@ using System.Collections;
 
 public class ShellSpawner : MonoBehaviour
 {
-	private GameObject shell;
+	private Transform shell;
 	private BoxCollider2D spawnerHitbox;
 	private GameObject player;
 
-	[SerializeField] private int maxShells = 1;
-	private int shellsSpawned = 0;
+    [SerializeField] private bool shouldSpawnNewShell = false;
+    [SerializeField] private int maxShells = 1;
+    private int shellsSpawned = 0;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
 	{
-		player = GameObject.Find ("snail");
+		player  = GameObject.Find ("snail");
+        shell   = player.transform.Find ("Shell");
 		Messenger.AddListener(GameEvent.SHELL_DESTROYED, SpawnShell);
 
 	}
@@ -23,22 +25,32 @@ public class ShellSpawner : MonoBehaviour
 	}
 	
 	// Update is called once per frame
-	private void OnTriggerEnter2D(Collider2D collision)
+	private void TriggerInteraction(Collider2D collision)
 	{
-		if (collision.gameObject.tag == "Player" && shellsSpawned < maxShells)
+		if (collision.gameObject.tag == "Player" && shellsSpawned <= maxShells && player.transform.Find("Shell") == null)
 		{
-			SpawnShell ();
+            if (shouldSpawnNewShell)
+                SpawnShell();
+            else
+                ReturnShell();
 		}
 	}
 
-	private void SpawnShell() {
-		//shell = GameObject.Find ("Shell");
-		GameObject newShell = (GameObject) Instantiate (Resources.Load("Shell"));
-		newShell.transform.position = this.transform.position;
-		shellsSpawned++;
+    private void SpawnShell()
+    {
+        GameObject newShell = (GameObject)Instantiate(Resources.Load("Shell"));
+        newShell.transform.position = this.transform.position;
+        shellsSpawned++;
 
-		// log that a shell spawner has been used and the position of the player
-		Managers.logging.RecordEvent (4, "" + player.transform.position);
-	}
+        //log that a shell spawner has been used and the position of the player
+  
+        Managers.logging.RecordEvent(4, "" + player.transform.position);
+    }
+
+    private void ReturnShell()
+    {
+        if (player.transform.Find("Shell") == null)
+        shell.position = this.transform.position;
+    }
 }
 
