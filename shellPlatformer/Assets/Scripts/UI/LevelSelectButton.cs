e55bf1ab67e8803a86614cb7bf968e6aec75d924
NickTeo96 <nt269@cornell.edu>
2017-11-10 16:05:09 -0500

    minor tweak to the levelbuttonprefab

--- shellPlatformer/Assets/Scripts/UI/LevelSelectButton.cs
+++ shellPlatformer/Assets/Scripts/UI/LevelSelectButton.cs
@@ -27,7 +27,6 @@ public class LevelSelectButton : MonoBehaviour {
 		Image image = GetComponent<Image> ();
 		image.sprite = unlocked;
 		levelText.text = "" + attachedLevel;
-		levelText.color = Color.black;
 		isLocked = false;
 	}
 }
