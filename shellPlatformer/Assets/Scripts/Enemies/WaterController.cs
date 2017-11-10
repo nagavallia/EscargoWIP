8608ef10941beb360ca9dbc532764f3da5034c66
NickTeo96 <nt269@cornell.edu>
2017-11-09 19:24:09 -0500

    Changed the particle launcher script and changed the faucet

---
+++ shellPlatformer/Assets/Scripts/Enemies/WaterController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.gameObject.tag == "Shell") {
+			collision.gameObject.GetComponent<Shell> ().FillShell (); // Call fill shell on the shell to turn into water shell 
+		}
+
+		if (collision.gameObject.tag == "Ground" || collision.gameObject.layer == LayerMask.NameToLayer("Background")) {
+			Destroy (this.gameObject);
+		}
+	}
+}
